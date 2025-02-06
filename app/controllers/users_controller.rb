17e3ef8232b31b901f78bfb66a518a01920d7f43
Daunel Augustin <daunel@mac.mynetworksettings.com>
2025-02-06 11:13:37 -0500

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user# Handle a successful save.
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation)
 end
